266f1db69d1f932d4c71f3e839673ddee0233217
Artem Polishchuk <tim77@users.noreply.github.com>
2019-01-22 14:15:07 +0200

    Added description for commands

--- reduce-gnome3-ram-usage.sh
+++ reduce-gnome3-ram-usage.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Disables abrtd daemon which needed for automatic crash reporting and libvirtd which needed for software like gnome-boxes.
 systemctl disable libvirtd abrtd
 
+# Masking PackageKit.
 systemctl mask packagekit
 
+# Masking evolution services which requires online accounts and tracker service for indexing files.
 systemctl --user mask evolution-addressbook-factory evolution-calendar-factory evolution-source-registry tracker-miner-apps tracker-miner-fs tracker-store
 
+# Removes GNOME Software and ABRT-GUI applet.
 sudo dnf remove gnome-software abrt-desktop -y
